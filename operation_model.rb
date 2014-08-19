6066234987d6fd7c985cccc3cebed82a04227d25
Brian Luan <brian.luan@migo.tv>
2014-08-19 15:31:35 +0800

    init a new template for writeing the db operation program.

---
+++ operation_model.rb
@@ -0,0 +1,77 @@
+require 'rubygems'    
+require 'active_record'    
+
+RUN_DIR=Dir.pwd
+
+ActiveRecord::Base.logger = Logger.new("#{RUN_DIR}/log/debug.log")
+ActiveRecord::Base.configurations = YAML::load(IO.read("#{RUN_DIR}/conf/database.yml"))
+ActiveRecord::Base.establish_connection(:mysql)
+
+class Psgc < ActiveRecord::Base
+    self.table_name = "PSGC"
+end
+
+
+class GeoType < ActiveRecord::Base
+    self.table_name = "operation.geo_type"
+
+    has_many :barangays,     foreign_key: "type_id"
+    has_many :citys,         foreign_key: "type_id"
+    has_many :provinces,     foreign_key: "type_id"
+    has_many :regions,       foreign_key: "type_id"
+end
+
+class Region < ActiveRecord::Base
+    self.table_name = "operation.region"
+
+    has_many :barangays,     foreign_key: "region_id"
+    has_many :citys,         foreign_key: "region_id"
+    has_many :provinces,     foreign_key: "region_id"
+end
+
+class Province < ActiveRecord::Base
+    self.table_name = "operation.province"
+    
+    has_many :barangays,     foreign_key: "province_id"
+    has_many :citys,         foreign_key: "province_id"
+    
+    belongs_to :region,      foreign_key: "region_id"
+end
+
+class City < ActiveRecord::Base
+    self.table_name = "operation.city"
+    has_many :barangays,     foreign_key: "city_id"
+    has_many :pois,          foreign_key: "city_id"
+    
+    belongs_to :province,    foreign_key: "province_id"
+    belongs_to :region,      foreign_key: "region_id"
+end
+
+class Barangay < ActiveRecord::Base
+    self.table_name = "operation.barangay"
+    has_many :pois,          foreign_key: "barangay_id"
+    
+    belongs_to :city,        foreign_key: "city_id"
+    belongs_to :province,    foreign_key: "province_id"
+    belongs_to :region,      foreign_key: "region_id"
+end
+
+class Poi < ActiveRecord::Base
+    self.table_name = "operation.poi"
+    has_many :stores,        foreign_key: "poi_id"
+    
+    belongs_to :city,        foreign_key: "city_id"
+    belongs_to :barangay,    foreign_key: "city_id"
+end
+
+class Store < ActiveRecord::Base
+    self.table_name = "operation.store"
+    
+    belongs_to :poi,        foreign_key: "poi_id"
+    belongs_to :store_type, foreign_key: "store_type_id"
+end
+
+class StoreType < ActiveRecord::Base
+    self.table_name = "operation.store_type"
+    has_many :stores,   foreign_key: "store_type_id"
+end
